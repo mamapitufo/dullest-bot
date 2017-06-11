58f47cbfc4cc4b5c9a17a11577689fddc538360d
Alberto Brealey-Guzman <alberto@feltfragments.com>
2017-06-11 20:39:15 +0100

    Add `twitter-api` dep and authenticate bot
    Read keys, tokens and secrets from the environment and authenticate the bot when
    the app runs.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://twitter.com/DullestBot"
   :license {:name "GNU General Public License"
             :url "https://www.gnu.org/licenses/#GPL"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [twitter-api "1.8.0"]]
   :main ^:skip-aot dullest-bot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/dullest_bot/core.clj
+++ src/dullest_bot/core.clj
@@ -1,5 +1,16 @@
 (ns dullest-bot.core
+  (:require [twitter.oauth :as oauth])
   (:gen-class))
+
+(defonce app-consumer-key         (System/getenv "DULLEST_BOT_CONSUMER_KEY"))
+(defonce app-consumer-secret      (System/getenv "DULLEST_BOT_CONSUMER_SECRET"))
+(defonce user-access-token        (System/getenv "DULLEST_BOT_USER_ACCESS_TOKEN"))
+(defonce user-access-token-secret (System/getenv "DULLEST_BOT_USER_ACCESS_TOKEN_SECRET"))
+
+(def creds (oauth/make-oauth-creds app-consumer-key
+                                   app-consumer-secret
+                                   user-access-token
+                                   user-access-token-secret))
 
 (defn -main
   "I don't do a whole lot ... yet."
